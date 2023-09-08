c7870e39045e4ba1f48535085203dd8a87d5c00c
Lopez-CL <cllopez511@gmail.com>
2023-09-08 08:16:17 -0400

    installed flask-session to help build context window

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,3 +1,9 @@
 from flask import Flask
+from flask_session import Session
 app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'  # Use the filesystem as the session storage
+app.config['SESSION_FILE_DIR'] = 'your_session_directory'  # Specify a directory to save session files
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_USE_SIGNER'] = True  # Secure your cookies
 app.secret_key = 'keepitsecretkeepitsafe'
+Session(app)
--- flask_app/controllers/chats.py
+++ flask_app/controllers/chats.py
@@ -1,7 +1,24 @@
 from flask_app import app
-from flask import request, render_template, redirect, session, flash
-# import os, requests
+from flask import request, render_template, redirect, session, flash, jsonify
+from flask_session import Session # import os, requests
+# My prompt completion method
+def get_completion():
+    print('yo')
 # Render page with chatbot interface
 @app.route('/')
 def render_chatbot():
+    session['messages'] = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    ]
+    print(session['messages'])
     return render_template('index.html')
+
+@app.route('/get/completion', methods = ['post'])
+def join_completion_with_messages():
+    user_message = request.form['user-prompt']
+    if 'messages' in session:
+        session['messages'].append({'role': 'user','content':user_message})
+        print(session['messages'])
+        result = jsonify(session['messages'])
+        return result
+    # results = get_completion()
