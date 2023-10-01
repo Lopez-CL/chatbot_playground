9a1acea74391d8780439109d36ec4f7f86e0ac70
Lopez-CL <cllopez511@gmail.com>
2023-10-01 09:47:36 -0400

    variable bug in chatBotServe.js fixed

--- flask_app/static/chatBotServe.js
+++ flask_app/static/chatBotServe.js
@@ -1,38 +1,40 @@
-// For chat start and serve
+// For chat start and serving responses
 const chatInput = document.getElementById('chat-input')
+const chatForm = document.getElementById('chatbot')
 const completionsContainer = document.getElementById('completions')
 let currentUserPrompt = ""
 const promptInput = e =>{
-    currentUserPrompt = e.currentTarget.value}
+    currentUserPrompt = e.currentTarget.value
+    console.log(currentUserPrompt)
+    }
 chatInput.addEventListener('input', promptInput)
 const chatResponse = e =>{
     e.preventDefault();
-    chatInput.value=""
     console.log(currentUserPrompt)
     if(!currentUserPrompt){
-        console.log("I'm more helpful when you start the conversation.")
+        console.log("I'm more helpful when you start the conversation.");
+        return null
     }
     let userMessage = document.createElement('p')
     userMessage.innerText = `${currentUserPrompt}`
-    let chatForm = document.getElementById('chatbot');
-    let newPrompt = new FormData(chatForm);
     completionsContainer.appendChild(userMessage)
+    let newPrompt = new FormData(chatForm);
     fetch("http://127.0.0.1:5000/get/completion", {method: 'post', body: newPrompt })
     .then(res => {
         if(!res.ok){throw new Error('Network response was not ok!')} 
+        chatInput.value=""
+        currentUserPrompt = ""
         return res.json()
     })
     .then(data =>{
         console.log(data)
-        for(let i = 0;  i < data.length; i++){ 
-            let botMessage = document.createElement('p')
-            botMessage.innerText =  `${data[i].content}`
-            botMessage.classList.add('text-black')
-            console.log(data[i].content)
-            console.log(botMessage)
-            console.log(`${data[i].content}`)
-            completionsContainer.appendChild(botMessage)
-        }
+        let botMessage = document.createElement('p')
+        botMessage.innerText =  `${data[0].content}`
+        botMessage.classList.add('text-black')
+        console.log(data[0].content)
+        console.log(botMessage)
+        console.log(`${data[0].content}`)
+        completionsContainer.appendChild(botMessage)
     })
     .catch(err => console.log(err))
 }
@@ -45,6 +47,7 @@ const clearSession = e =>{
         if(!res.ok){throw new Error('Network response was not ok')}
         completionsContainer.innerHTML= "";
         chatInput.value=""
+        currentUserPrompt = ""
     })
 }
 clearChatBtn.addEventListener('click', clearSession)
